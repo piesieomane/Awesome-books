986373b202134877ec78ad59914f1d79ce988290
Anas Abubakar <anasabubakar2015@gmail.com>
2022-06-27 15:51:35 +0100

    Fix linters

--- books.js
+++ books.js
@@ -1,10 +1,11 @@
-const btnAdd = document.querySelector(".submit");
-const title = document.querySelector(".title");
-const author = document.querySelector(".author");
-const bookStore = document.querySelector(".books-collection");
+const btnAdd = document.querySelector('.submit');
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const bookStore = document.querySelector('.books-collection');
 const collection = [];
+/* eslint-disable no-use-before-define */
 
-btnAdd.addEventListener("click", (e) => {
+btnAdd.addEventListener('click', (e) => {
   e.preventDefault();
   if (title.value && author.value) {
     const book = {
@@ -15,40 +16,40 @@ btnAdd.addEventListener("click", (e) => {
     collection.push(book);
 
     renderBooks();
-    title.value = "";
-    author.value = "";
+    title.value = '';
+    author.value = '';
   }
 });
 
 const renderBooks = () => {
-  bookStore.innerHTML = "";
+  bookStore.innerHTML = '';
   collection.forEach((item) => {
-    const singleBook = document.createElement("div");
-    singleBook.classList.add("class-book");
-    const p = document.createElement("p");
+    const singleBook = document.createElement('div');
+    singleBook.classList.add('class-book');
+    const p = document.createElement('p');
     p.textContent = `${item.title} By ${item.author}`;
-    const bton = document.createElement("button");
-    bton.textContent = `Remove`;
-    bton.classList.add("remove");
-    bton.setAttribute("id", item.id);
+    const bton = document.createElement('button');
+    bton.textContent = 'Remove';
+    bton.classList.add('remove');
+    bton.setAttribute('id', item.id);
     singleBook.appendChild(p);
     singleBook.appendChild(bton);
     bookStore.appendChild(singleBook);
   });
 };
 
-//Use Event bubbling
-bookStore.addEventListener("click", (e) => {
-  if (e.target.classList.contains("remove")) {
-    const targetid = parseInt(e.target.getAttribute("id"));
-    console.log("id", targetid);
+// Use Event bubbling
+bookStore.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove')) {
+    const targetid = parseInt(e.target.getAttribute('id'));
+    console.log('id', targetid);
     removeBook(targetid);
   }
 });
 
 function removeBook(targetId) {
   const newArr = collection.filter((item) => {
-    console.log("dhd", item.id, targetId);
+    console.log('dhd', item.id, targetId);
     return item.id !== targetId;
   });
   console.log(newArr);
@@ -56,3 +57,4 @@ function removeBook(targetId) {
   collection.push(...newArr);
   renderBooks();
 }
+
