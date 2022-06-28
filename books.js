cf1eb552ae018a3f5b7dd9b9859c72baed73fe23
piesieomane <piesieyeboah@gmail.com>
2022-06-28 18:19:38 +0300

    refactor classes and style book

--- books.js
+++ books.js
@@ -1,7 +1,7 @@
-const btnAdd = document.querySelector('.submit');
-const title = document.querySelector('.title');
-const author = document.querySelector('.author');
-const bookStore = document.querySelector('.books-collection');
+const btnAdd = document.querySelector(".submit");
+const title = document.querySelector(".title");
+const author = document.querySelector(".author");
+const bookStore = document.querySelector(".books-collection");
 // const collection = JSON.parse(localStorage.getItem('form')) || [];
 /* eslint-disable no-use-before-define */
 
@@ -54,45 +54,71 @@ const bookStore = document.querySelector('.books-collection');
 //   renderBooks();
 // }
 class Book {
-  constructor(title, author){
+  constructor(title, author) {
     this.title = title;
     this.author = author;
-    this.id = Date.now;
+    this.id = Date.now();
   }
-  collection = JSON.parse(localStorage.getItem('form')) || [];
-  renderBooks = () => {
-      bookStore.innerHTML = '';
-      collection.forEach((item) => {
-        const singleBook = document.createElement('div');
-        singleBook.classList.add('class-book');
-        const p = document.createElement('p');
-        p.textContent = `${item.title} By ${item.author}`;
-        const bton = document.createElement('button');
-        bton.textContent = 'Remove';
-        bton.classList.add('remove');
-        bton.setAttribute('id', item.id);
-        singleBook.appendChild(p);
-        singleBook.appendChild(bton);
-        bookStore.appendChild(singleBook);
-      });
-    };
 
-    removeBook(targetId) {
-        const newArr = collection.filter((item) => item.id !== targetId);
-        collection.length = 0;
-        collection.push(...newArr);
-        localStorage.setItem('form', JSON.stringify(collection));
-        renderBooks();
-      }
+  collection = JSON.parse(localStorage.getItem("form")) || [];
+
+  renderBooks() {
+    this.collection = JSON.parse(localStorage.getItem("form")) || [];
+    bookStore.innerHTML = "";
+    if (!this.collection) return;
+    this.collection.forEach((item) => {
+      const singleBook = document.createElement("div");
+      singleBook.classList.add("class-book");
+      const p = document.createElement("p");
+      p.textContent = `"${item.title}" by ${item.author}`;
+      const bton = document.createElement("button");
+      bton.textContent = "Remove";
+      bton.classList.add("remove");
+      bton.setAttribute("id", item.id);
+      singleBook.appendChild(p);
+      singleBook.appendChild(bton);
+      bookStore.appendChild(singleBook);
+    });
+  }
+
+  adddBook() {
+    this.collection = JSON.parse(localStorage.getItem("form")) || [];
+    const book = {
+      title: this.title,
+      author: this.author,
+      id: this.id,
+    };
+    this.collection.push(book);
+    localStorage.setItem("form", JSON.stringify(this.collection));
+  }
+
+  removeBook(targetId) {
+    this.collection = JSON.parse(localStorage.getItem("form")) || [];
+    const newArr = this.collection.filter((item) => item.id !== targetId);
+    this.collection.length = 0;
+    this.collection.push(...newArr);
+    localStorage.setItem("form", JSON.stringify(this.collection));
+    this.renderBooks();
+  }
 }
 
-const newBook = new Book(title.value, author.value, Date.now())
-newBook.renderBooks();
-newBook.removeBook();
-bookStore.addEventListener('click', (e) => {
-    if (e.target.classList.contains('remove')) {
-      const targetid = parseInt(e.target.getAttribute('id'), 10);
-      removeBook(targetid);
-    }
-  });
-  
+const miscBook = new Book();
+
+miscBook.renderBooks();
+bookStore.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove")) {
+    const targetid = parseInt(e.target.getAttribute("id"), 10);
+    miscBook.removeBook(targetid);
+  }
+});
+
+btnAdd.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!title.value || !author.value) return;
+
+  const newBook = new Book(title.value, author.value, Date.now());
+  newBook.adddBook();
+  newBook.renderBooks();
+  title.value = "";
+  author.value = "";
+});
